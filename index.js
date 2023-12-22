ba04f15a8acc28e62d75f11ed05118bc2e936a17
Amit kumar <ramitgzp56@gmail.com>
2023-12-22 15:18:18 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app.js');
 const mongoose = require('mongoose');
 
 const mongoDB = async () => {
-    await mongoose.connect("mongodb://localhost:27017/wti")
+    await mongoose.connect("mongodb+srv://ramitgzp56:uQ9Z9riDDbsW7Px0@cluster0.3wpkrlc.mongodb.net/")
 }
 mongoDB().then(()=>{
     console.log("mongodb connected......")
